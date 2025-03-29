3dbd8d90d8333b50dacc39132c8943652ce6d98a
alina <155826828+alienmasterr@users.noreply.github.com>
2025-03-29 18:23:52 +0200

    lab02 ready

---
+++ MainViewModel.cs
@@ -0,0 +1,128 @@
+﻿using Lab_CSharp.Helpers;
+using Lab_CSharp.Models;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Lab_CSharp.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private DateTime? _birthDate;
+        private string _resultText;
+        private bool _isProcessing;
+
+        public string FirstName
+        {
+            get => _firstName;
+            set { _firstName = value; OnPropertyChanged(nameof(FirstName)); OnPropertyChanged(nameof(CanProceed)); }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set { _lastName = value; OnPropertyChanged(nameof(LastName)); OnPropertyChanged(nameof(CanProceed)); }
+        }
+
+        public string Email
+        {
+            get => _email;
+            set { _email = value; OnPropertyChanged(nameof(Email)); OnPropertyChanged(nameof(CanProceed)); }
+        }
+
+        public DateTime? BirthDate
+        {
+            get => _birthDate;
+            set { _birthDate = value; OnPropertyChanged(nameof(BirthDate)); OnPropertyChanged(nameof(CanProceed)); }
+        }
+
+        public string ResultText
+        {
+            get => _resultText;
+            set { _resultText = value; OnPropertyChanged(nameof(ResultText)); }
+        }
+
+        public bool IsProcessing
+        {
+            get => _isProcessing;
+            set { _isProcessing = value; OnPropertyChanged(nameof(IsProcessing)); }
+        }
+
+        public bool CanProceed =>
+            !string.IsNullOrWhiteSpace(FirstName) &&
+            !string.IsNullOrWhiteSpace(LastName) &&
+            !string.IsNullOrWhiteSpace(Email) &&
+            BirthDate.HasValue;
+
+        public ICommand ProceedCommand { get; }
+
+        public MainViewModel()
+        {
+            ProceedCommand = new RelayCommand(async obj => await ExecuteProceedAsync(), obj => CanProceed);
+        }
+
+        private async Task ExecuteProceedAsync()
+        {
+            IsProcessing = true;
+
+            // await Task.Delay(1000);
+            if (!BirthDate.HasValue) return;
+
+            var person = new Person(FirstName, LastName, Email, BirthDate.Value);
+
+
+            int age = person.CalculateAge(BirthDate.Value);
+
+            if (age < 0)
+            {
+                MessageBox.Show("You haven't been born yet!", "Error!!!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (age > 135)
+            {
+                MessageBox.Show("You are dead!", "Error!!!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+
+            if (!Email.Contains("@"))
+            {
+                ResultText = "Error: Email must have @!";
+                IsProcessing = false;
+                return;
+            }
+
+
+            ResultText = $"Name: {person.FirstName}\n" +
+                         $"Surname: {person.LastName}\n" +
+                         $"Email: {person.Email}\n" +
+                         $"Birth date: {person.BirthDate.ToShortDateString()}\n" +
+                         $"Age: {age} years old\n" +
+                         $"is Adult: {person.IsAdult}\n" +
+                         $"Western zodiac: {person.SunSign}\n" +
+                         $"Chineese zodiac: {person.ChineseSign}\n" +
+                         $"Has birthday today: {person.IsBirthday}";
+
+            if (person.IsBirthday)
+            {
+                MessageBox.Show($"Happy birthday, {FirstName}! 🎉", "Congrats!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
+            IsProcessing = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
+
---
+++ Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Lab_CSharp.Models
+{
+    public class Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string Email { get; }
+        public DateTime BirthDate { get; }
+
+        public bool IsAdult => (DateTime.Now - BirthDate).TotalDays / 365.25 >= 18;
+        public bool IsBirthday => BirthDate.Day == DateTime.Now.Day && BirthDate.Month == DateTime.Now.Month;
+        public string SunSign => GetSunSign();
+        public string ChineseSign => GetChineseSign();
+
+        public Person(string firstName, string lastName, string email, DateTime birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public Person(string firstName, string lastName, string email)
+            : this(firstName, lastName, email, DateTime.MinValue) { }
+
+        public Person(string firstName, string lastName, DateTime birthDate)
+            : this(firstName, lastName, null, birthDate) { }
+
+        private string GetSunSign()
+        {
+            int day = BirthDate.Day;
+            int month = BirthDate.Month;
+            return month switch
+            {
+                1 => day <= 19 ? "Capricorn" : "Aquarius",
+                2 => day <= 18 ? "Aquarius" : "Pisces",
+                3 => day <= 20 ? "Pisces" : "Aries",
+                4 => day <= 19 ? "Aries" : "Taurus",
+                5 => day <= 20 ? "Taurus" : "Gemini",
+                6 => day <= 20 ? "Gemini" : "Cancer",
+                7 => day <= 22 ? "Cancer" : "Leo",
+                8 => day <= 22 ? "Leo" : "Virgo",
+                9 => day <= 22 ? "Virgo" : "Libra",
+                10 => day <= 22 ? "Libra" : "Scorpius",
+                11 => day <= 21 ? "Scorpius" : "Sagittarius",
+                12 => day <= 21 ? "Sagittarius" : "Capricorn",
+                _ => "Unkown"
+            };
+        }
+
+        private string GetChineseSign()
+        {
+            string[] signs = { "Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat" };
+
+            return signs[BirthDate.Year % 12];
+        }
+
+
+        public int CalculateAge(DateTime birthDate)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - birthDate.Year;
+            if (birthDate > today.AddYears(-age)) age--; // Якщо день народження ще не настав цього року
+            return age;
+        }
+    }
+}
+
+
